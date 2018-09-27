2d670bdf9da0af1aa51d642f61f885f5879cadec
Pierre SYLVESTRE <pierre.s@onthegosystems.com>
2018-09-27 10:32:03 -0300

    Implement media usage for non-translatable blocks
    https://onthegosystems.myjetbrains.com/youtrack/issue/wpmlcore-5834

--- src/media/class-wpml-beaver-builder-media-node-provider.php
+++ src/media/class-wpml-beaver-builder-media-node-provider.php
@@ -8,6 +8,10 @@ class WPML_Beaver_Builder_Media_Node_Provider {
 	/** @var WPML_Beaver_Builder_Media_Node[] */
 	private $nodes = array();
 
+	public function __construct( WPML_Page_Builders_Media_Translate $media_translate ) {
+		$this->media_translate = $media_translate;
+	}
+
 	/**
 	 * @param string $type
 	 *
@@ -17,19 +21,19 @@ class WPML_Beaver_Builder_Media_Node_Provider {
 		if ( ! array_key_exists( $type, $this->nodes ) ) {
 			switch ( $type ) {
 				case 'photo':
-					$node = new WPML_Beaver_Builder_Media_Node_Photo( $this->get_media_translate() );
+					$node = new WPML_Beaver_Builder_Media_Node_Photo( $this->media_translate );
 					break;
 
 				case 'gallery':
-					$node = new WPML_Beaver_Builder_Media_Node_Gallery( $this->get_media_translate() );
+					$node = new WPML_Beaver_Builder_Media_Node_Gallery( $this->media_translate );
 					break;
 
 				case 'content-slider':
-					$node = new WPML_Beaver_Builder_Media_Node_Content_Slider( $this->get_media_translate() );
+					$node = new WPML_Beaver_Builder_Media_Node_Content_Slider( $this->media_translate );
 					break;
 
 				case 'slideshow':
-					$node = new WPML_Beaver_Builder_Media_Node_Slideshow( $this->get_media_translate() );
+					$node = new WPML_Beaver_Builder_Media_Node_Slideshow( $this->media_translate );
 					break;
 
 				default:
@@ -40,19 +44,5 @@ class WPML_Beaver_Builder_Media_Node_Provider {
 		}
 
 		return $this->nodes[ $type ];
-	}
-
-	/** @return WPML_Page_Builders_Media_Translate */
-	private function get_media_translate() {
-		global $sitepress;
-
-		if ( ! $this->media_translate ) {
-			$this->media_translate = new WPML_Page_Builders_Media_Translate(
-				new WPML_Translation_Element_Factory( $sitepress ),
-				new WPML_Media_Image_Translate( $sitepress, new WPML_Media_Attachment_By_URL_Factory() )
-			);
-		}
-
-		return $this->media_translate;
 	}
 }
